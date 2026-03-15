410425699bcca881bd20a8d71b3399e006f92f34
Abdoubel26 <abdoubelounis09@gmail.com>
2026-03-15 15:25:10 +0100

    removed import bug

--- backend/routes/messages.route.js
+++ backend/routes/messages.route.js
@@ -1,11 +1,9 @@
 import express from 'express'
-import { getMessages, sendMessage } from '../controllers/MessageControllers.js'
+import { getMessages } from '../controllers/MessageControllers.js'
 const router = express.Router()
 import { AuthMiddleware } from '../middleware/auth.js'
 
 router.get('/', AuthMiddleware, getMessages)
-
-router.post('/', AuthMiddleware, sendMessage)
 
 
 export default router
